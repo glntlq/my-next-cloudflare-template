9102ee531252c56eb4d1e05a838277cca0a66848
Gao Lining <lngao@best-inc.com>
2025-08-07 18:28:11 +0800

    i18n

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,11 +1,16 @@
+'use client'
+
+import { useTranslations } from 'next-intl'
 import Image from 'next/image'
-import Link from 'next/link'
+
+import { Link } from '@/i18n/navigation'
 
 const Logo = () => {
+  const t = useTranslations('siteInfo')
   return (
     <Link href="/" className="flex items-center gap-2">
       <Image alt="logo" src="/logo.svg" width={32} height={32} />
-      <p className="text-xl font-semibold">Next Template</p>
+      <p className="text-xl font-semibold">{t('brandName')}</p>
     </Link>
   )
 }
